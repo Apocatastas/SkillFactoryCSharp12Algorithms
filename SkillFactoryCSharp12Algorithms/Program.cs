855c468cd66434f6d0d20af5de0f44e8de7dbfff
ɐllɐ <molchanova.a.a@gmail.com>
2024-10-31 16:51:43 +0300

    User class added

--- SkillFactoryCSharp12Algorithms/Program.cs
+++ SkillFactoryCSharp12Algorithms/Program.cs
@@ -1,18 +1,35 @@
 ﻿using System;
-
-class MainClass
+namespace SkillFactoryCSharp12Algorithms
 {
-    public static void Main(string[] args)
+    class MainClass
     {
-        Console.WriteLine("Введите свой возраст");
-        int age = Convert.ToInt32(Console.ReadLine());
-        if (age > 13)
+        public void Main(string[] args)
         {
-            Console.WriteLine("Вы успешно зарегистрированы");
+            List<User> userList = new List<User>();
+            GetUserList(userList);
+            foreach (User user in userList)
+            {
+                Console.WriteLine("Привет, " + user.Name);
+                if (!user.IsPremium)
+                {
+                    ShowAds();
+                }
+            }
         }
-        else
+
+        public void ShowAds()
         {
-            Console.WriteLine("Пользователи младше 14 лет не могут быть зарегистрированы");
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            Thread.Sleep(1000);
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            Thread.Sleep(2000);
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            Thread.Sleep(3000);
+        }
+
+        public void GetUserList(List<User> list)
+        {
+
         }
     }
 }
---
+++ SkillFactoryCSharp12Algorithms/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace SkillFactoryCSharp12Algorithms
+{
+	public class User
+	{
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public User(string login, string name, bool status)
+        {
+            Login = login;
+            Name = name;
+            IsPremium = status;
+        }
+	}
+}
+
