ef973e4b2770f701e9e239fdb9719bc6ed4537f8
ɐllɐ <molchanova.a.a@gmail.com>
2024-10-31 10:48:24 +0300

    task 12.1.2

--- SkillFactoryCSharp12Algorithms/Program.cs
+++ SkillFactoryCSharp12Algorithms/Program.cs
@@ -4,6 +4,9 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-
+        Console.WriteLine("Как вас зовут?");
+        string name = Console.ReadLine();
+        string greetings = "Привет, " + name;
+        Console.WriteLine(greetings);
     }
 }
