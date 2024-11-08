882e63a71a34ef503d4af80b923c04a8f5fec2a2
ɐllɐ <molchanova.a.a@gmail.com>
2024-11-08 22:10:57 +0300

    task 12.3.1 - binary search

--- TasksInUnit/Program.cs
+++ TasksInUnit/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Reflection;
 
 namespace TasksInUnit
 {
@@ -6,34 +8,58 @@ namespace TasksInUnit
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Введите размер массива: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] array = new int[length];
+            Random rand = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = rand.Next(-50, 100);
+            }
+
+            Console.WriteLine("Несортированный массив:");
+            PrintArray(array, array.Length);
+
+            Array.Sort(array, 0, length);
+
+            Console.WriteLine("\nОтсортированный массив:");
+            PrintArray(array, array.Length);
+
+            Console.WriteLine("\nВведите элемент для поиска: ");
+            int target = int.Parse(Console.ReadLine());
+            int targetPosition = BinarySearch(array, target, 0, array.Length - 1);
+            Console.WriteLine("Найден, элемент array[{0}] = {1}", targetPosition, array[targetPosition]);
+            Console.ReadKey();
         }
 
-        static void GetPageForUser(User user)
+        public static void PrintArray(int[] array, int length)
         {
-            if (!user.LoggedIn) return;
-            Console.WriteLine("User logged in");
-            if (!user.HasAccessToSection) return;
-            Console.WriteLine("User has access to that section");
-
-                    if (user.HasUnseenNews)
-                    {
-                        ShowNews();
-                    }
-
-                    if (user.HasAdminRights)
-                    {
-                        DecorateAdminTools();
-                    }
-
-
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
         }
 
-        static void ShowNews()
+        public static int BinarySearch(int[] array, int target, int left, int right)
         {
-        }
+            int middle = array.Length / 2;
+            while (!(array[middle] == target))
+            {
+                middle = (left + right) / 2;
+                if (array[middle] > target)
+                {
+                    Console.WriteLine("Ищем: {0} - {1}", left, right);
+                    right = middle - 1;
 
-        static void DecorateAdminTools()
-        {
+                }
+                else
+                {
+                    Console.WriteLine("Ищем: {0} - {1}", left, right);
+                    left = middle + 1;
+                }
+
+            }
+            return middle;
         }
     }
 }
