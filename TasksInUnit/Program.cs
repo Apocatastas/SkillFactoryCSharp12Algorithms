6dd9f82df3548332f43bde7f20d6bbb9a57fe6fe
ɐllɐ <molchanova.a.a@gmail.com>
2024-11-12 21:06:32 +0300

    profiler check

--- TasksInUnit/Program.cs
+++ TasksInUnit/Program.cs
@@ -1,13 +1,40 @@
 ﻿using System;
-using BenchmarkDotNet.Running;
 
 namespace TasksInUnit
 {
-    class MainClass
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-                var summary = BenchmarkRunner.Run<Testing>();
+            Estimate(20);
+
+            Console.ReadKey();
+        }
+
+        static void CreateMatrix(int n)
+        {
+            var matrix = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = new int[n];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i][j] = i + j;
+                }
+            }
+        }
+
+        static void Estimate(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                CreateMatrix(10000);
+            }
         }
     }
 }
